5a93d6b7b8987a808fdbe2b67f7a8bf67c41c1a8
joshua bowles <bowlesling@gmail.com>
2015-11-23 20:58:45 -0700

    reformat documentation

--- vec_model.go
+++ vec_model.go
@@ -88,24 +88,24 @@ func (vs VocabSlice) Swap(i, j int) {
 VectorModel struct contains fields needs to train a wrod2vec or word2phrase model:
 
 Required:
-	`OutputFile`      REQUIRED. Use <file> to save the resulting word vectors / word clusters.
-	`TrainFile`		  REQUIRED. Use text data from <file> to train the model.
+	OutputFile	  REQUIRED. Use <file> to save the resulting word vectors / word clusters.
+	TrainFile	  REQUIRED. Use text data from <file> to train the model.
 
 Optional (Option functions are supported):
-	`Alpha`			  Sets the starting learning rate; default is 0.025 for skip-gram,  and 0.05 for CBOW.
-	`Binaryf`		  Decides if the resulting vectors in binary file; default is false (off).
-	`Cbow`			  Uses the continuous bag of words model; default is true (use false for skip-gram model).
-	`DebugMode`		  Sets the debug mode (default = 2 = more info during training).
-	`InVocabFile`	  The vocabulary will be read from <file>, not constructed from the training data, if "" then program will generate vocab. Default is "".
-	`Iter`			  Is the number of iterations of training.
-	`KmeansClasses`	  Will output word classes rather than word vectors; default number of classes is 0 (vectors are written).
-	`Layer1VecSize`	  Sets size of word vectors; default is 100.
-	`MinCount`		  This will discard words that appear less than n times; default is 5.
-	`NegSampling`	  Number of negative examples; default is 5, common values are 3 - 10 (0 = not used).
-	`OutVocabFile`	  The vocabulary will be saved to <file>; if no file name given, i.e. "", then it won't be saved.
-	`Sample`		  Sets threshold for occurrence of words. Those that appear with higher frequency in the training data will be randomly down-sampled; default is 1e-3, useful range is (0, 1e-5).
-	`SoftMax`		  Use Hierarchical Softmax; default is false (not used).
-	`WindowSkipLen`	  Set max skip length between words; default is 5.
+	Alpha		  Sets the starting learning rate; default is 0.025 for skip-gram,  and 0.05 for CBOW.
+	Binaryf		  Decides if the resulting vectors in binary file; default is false (off).
+	Cbow		  Uses the continuous bag of words model; default is true (use false for skip-gram model).
+	DebugMode	  Sets the debug mode (default = 2 = more info during training).
+	InVocabFile	  The vocabulary will be read from <file>, not constructed from the training data, if "" then program will generate vocab. Default is "".
+	Iter		  Is the number of iterations of training.
+	KmeansClasses Will output word classes rather than word vectors; default number of classes is 0 (vectors are written).
+	Layer1VecSize Sets size of word vectors; default is 100.
+	MinCount	  This will discard words that appear less than n times; default is 5.
+	NegSampling	  Number of negative examples; default is 5, common values are 3 - 10 (0 = not used).
+	OutVocabFile  The vocabulary will be saved to <file>; if no file name given, i.e. "", then it won't be saved.
+	Sample		  Sets threshold for occurrence of words. Those that appear with higher frequency in the training data will be randomly down-sampled; default is 1e-3, useful range is (0, 1e-5).
+	SoftMax		  Use Hierarchical Softmax; default is false (not used).
+	WindowSkipLen Set max skip length between words; default is 5.
 */
 type VectorModel struct {
 	Alpha           float64
@@ -150,8 +150,8 @@ type VectorModel struct {
 
 // PrecomputeExpTable builds the computes an exponent table using EXP_TABLE_SIZE and MAX_EXP
 func PreComputeExpTable() (expTable []float64) {
-	//expTable = make([]float64, int(EXP_TABLE_SIZE+1))
-	expTable = make([]float64, 2)
+	expTable = make([]float64, int(EXP_TABLE_SIZE+1))
+	//expTable = make([]float64, EXP_TABLE_SIZE)
 
 	for i := 0; i < int(EXP_TABLE_SIZE); i++ {
 		expTable[i] = math.Exp((float64(i) / EXP_TABLE_SIZE * (2 - 1)) * MAX_EXP) // Precompute the exp() table
@@ -160,7 +160,9 @@ func PreComputeExpTable() (expTable []float64) {
 	return
 }
 
-// NewPhraseVectorModel creates the word vector model struct for running word2phrase modelling. It does not requires the amount of parameters for word2vec and therefore the struct is much smaller and uses only a few of the fields. See NewWord2VecModel for how to use the ModelParams variadic arg.
+/*
+NewWord2PhraseModel creates the word vector model struct for running word2phrase modelling. It does not require the amount of parameters for word2vec and therefore the struct is much smaller and uses only a few of the fields. See NewWord2VecModel for how to use the ModelParams variadic arg.
+*/
 func NewWord2PhraseModel(trainFile string, outFile string, threshold float64, modelParams ...ModelParams) (*VectorModel, error) {
 	vm := &VectorModel{
 		TrainFile:     trainFile,
@@ -188,23 +190,21 @@ func NewWord2PhraseModel(trainFile string, outFile string, threshold float64, mo
 	return vm, nil
 }
 
-/* NewWord2VecModel creates a new WordVecModel for the word2vec technique. It offers many arguments to modify paramters to the model. Many arguments will not need to be defined and can use the defaults. See vec_model_test for more examples.
+/*
+NewWord2VecModel creates a new WordVecModel for word2vec algorithms. It offers many arguments to modify paramters to the model.
 
 An example with zero learning rate, no debugging output, doing 2o iterations and using a vocab input file:
-  var ZeroLearningRate ModelParams = AlphaOption(0)
-  var NoDebug ModelParams = DebugModeOption(0)
-  var TwentyIters ModelParams = IterOption(20)
-  var VocabFileInput ModelParams = InVocabFileOption("input_vocab_file.dat")
-
-  wvm := NewWord2VecModel("training_data.txt", "word2vec_file.txt", ZeroLearningRate, NoDebug, TwentyIters, BinaryFileTrue, BagOfWordsFalse, VocabFileInput)
-
+	var ZeroLearningRate ModelParams = AlphaOption(0)
+	var NoDebug ModelParams = DebugModeOption(0)
+	var TwentyIters ModelParams = IterOption(20)
+	var VocabFileInput ModelParams = InVocabFileOption("input_vocab_file.dat")
+	wvm := NewWord2VecModel("training_data.txt", "word2vec_file.txt", ZeroLearningRate, NoDebug, TwentyIters, BinaryFileTrue, BagOfWordsFalse, VocabFileInput)
 
 An example using k-means classes for words instead of vectors:
+	wvm := NewWord2VecModel("training_data.txt", "word2vec_kmeans_file.txt", )
 
-  wvm := NewWord2VecModel("training_data.txt", "word2vec_kmeans_file.txt", )
-
+Many arguments will not need to be defined and can rely on the defaults. See the tests for VecModel for more examples.
 */
-
 func NewWord2VecModel(trainFile, outFile string, modelParams ...ModelParams) (*VectorModel, error) {
 	vm := &VectorModel{
 		Alpha:           ALPHA_CBOW,
